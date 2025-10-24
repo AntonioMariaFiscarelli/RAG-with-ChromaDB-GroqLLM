b3ca437a1bd92b7feb39e834d40434dd49a18136
AntonioMariaFiscarelli <antoniofisk90@gmail.com>
2025-10-24 18:14:15 +0200

    main.py updated

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ from src.rag_search import RAGSearch
 embedding_manager = EmbeddingManager()
 vectorstore=VectorStore()
 
-if False:
-   docs = load_all_documents("data")
-   ## Generate the Embeddings
-   chunks = embedding_manager.chunk_documents(docs)
-   texts=[doc.page_content for doc in chunks]
-   embeddings = embedding_manager.generate_embeddings(texts)
 
-   vectorstore.add_documents(chunks, embeddings)
+docs = load_all_documents("data")
+## Generate the Embeddings
+chunks = embedding_manager.chunk_documents(docs)
+texts=[doc.page_content for doc in chunks]
+embeddings = embedding_manager.generate_embeddings(texts)
+
+vectorstore.add_documents(chunks, embeddings)
 
 
 rag_retriever=RAGRetriever(vectorstore, embedding_manager)
